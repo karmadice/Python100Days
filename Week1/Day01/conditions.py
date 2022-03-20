7da85bdfe233905a817c720f22128ef9fc16685f
krutarth patel <5004048+karmadice@users.noreply.github.com>
2022-03-20 02:05:33 +0000

    conditional operators

---
+++ Week1/Day01/conditions.py
@@ -0,0 +1,9 @@
+if((4<5) and (5 <6)):
+  print("That was true")
+else:
+  print("Well, it was worth it.")
+
+if((4<5) or (5 > 6)):
+  print("At least one condition is right")
+else:
+  print("Both conditions were wrong")
